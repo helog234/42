4170e8e4f38a1b427652dc44a0b11c6ff72e4b97
Hélène Gandar <helenegandar@ik.me>
2023-10-13 20:04:59 +0200

    libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,9 +6,36 @@
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:02:49 by hgandar           #+#    #+#             */
-/*   Updated: 2023/10/13 19:04:37 by hgandar          ###   ########.fr       */
+/*   Updated: 2023/10/13 20:04:34 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+CFILES = t_isalpha.c, ft_isdigit.c, ft_isalnum.c, ft_isascii.c, \
+         ft_isprint.c, ft_strlen.c, ft_memset.c, ft_bzero.c, \
+         ft_memcpy.c, ft_memmove.c, ft_strlcpy.c, ft_strlcat.c, \
+         ft_toupper.c, ft_tolower.c, ft_strchr.c, ft_strrchr.c, \
+         ft_strcmp.c, ft_memcmp.c, ft_strnstr.c, ft_atoi.c
+
+OFILES = $(SRCS:.c=.o)
+
+CFLAGS = -Wall -Wextra -Werror
+
+NAME = libft.a
+
+all: $(NAME) clean
+
+$(NAME): $(OFILES)
+     ar rcs $(NAME) $(OFILES) 
+
+clean:
+  rm -f $(OFILES)
+
+fclean:  
+  clean rm -f $(NAME)
+
+re: fclean $(NAME)
+
+.PHONY:  all clean fclean re
 
 #ifndef __LIBFT__
 #define __LIBFT__
