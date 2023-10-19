08f62eb2f0d2db7116078977be71b2b82bdf749a
Helene Gandar <helenegandar@ik.me>
2023-10-19 15:37:55 +0200

    extremis

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 10:08:39 by hgandar           #+#    #+#             */
-/*   Updated: 2023/10/18 11:58:01 by hgandar          ###   ########.fr       */
+/*   Updated: 2023/10/19 15:37:12 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(const char *str1)
 	int		i;
 
 	i = 0;
-	str2 = malloc (strlen(str1));
+	str2 = (char *) malloc (strlen(str1 + 1));
 	while (str1[i])
 	{
 		str2[i] = str1[i];
@@ -32,7 +32,8 @@ char	*ft_strdup(const char *str1)
 #include <stdio.h>
 int	main(void)
 {
-	char	str[] = "Hello Wolrd !";
+	char	str[] = "lorem ipsum dolor sit amet !";
+	char	*str_dup;
 	printf("Copy : %p\n", ft_strdup(str));
 	printf("%s\n", ft_strdup(str));
 	printf("Copy : %p\n", strdup(str));
