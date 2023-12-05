75f382fe7f1aa474c530e752e34d7d8cc0b2b9ff
Hélène Gandar <helenegandar@ik.me>
2023-12-05 16:55:00 +0100

    Debut de comprehension

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,12 +6,12 @@
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:34:46 by hgandar           #+#    #+#             */
-/*   Updated: 2023/11/29 16:30:39 by hgandar          ###   ########.fr       */
+/*   Updated: 2023/12/05 11:44:10 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef PIPEX_H
+# define PIPEX_H
 
 # include <stdlib.h>
 # include <unistd.h>
--- pipex/utils.c
+++ pipex/utils.c
@@ -1,24 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/05 14:38:39 by hgandar           #+#    #+#             */
-/*   Updated: 2023/12/05 14:40:07 by hgandar          ###   ########.fr       */
+/*   Created: 2023/12/05 16:28:50 by hgandar           #+#    #+#             */
+/*   Updated: 2023/12/05 16:33:37 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include "../libft/libft.h"
 #include "pipex.h"
-#include <fcntl.h>
+#include <string.h>
 
-int	main(char argc, char *argv[], char * envp[])
+char	*ft_strstr(const char *hstack, const char *needle)
 {
-	
+	unsigned long	i;
+	unsigned long	j;
+
+	i = 0;
+	j = 0;
+	if (needle[i] == 0 || needle[j] == 0)
+		return ((char *)hstack);
+	if (hstack == NULL)
+        return NULL;
+	while (hstack[i])
+	{
+		j = 0;
+		while (needle[j] && needle[j] == hstack[i + j])
+			j++;
+		if (needle[j] == 0)
+			return ((char *)&hstack[i]);
+		i++;
+	}
+	return (0);
 }
