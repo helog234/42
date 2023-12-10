7212f3ecaebcc2f9c6016b744b1ae41aaec156e2
Hélène Gandar <helenegandar@ik.me>
2023-12-10 11:18:35 +0100

    correction makefile pour clean

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,78 @@
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 10:21:55 by hgandar           #+#    #+#             */
-/*   Updated: 2023/12/10 10:21:56 by hgandar          ###   ########.fr       */
+/*   Updated: 2023/12/10 10:55:42 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+#include "pipex.h"
+#include "gnl/get_next_line_bonus.h"
+#include <fcntl.h>
+
+void	execute(char *argv, char *envp[])
+{
+	char	**cmd_split;
+	char	**env_paths;
+	char	*path;
+
+	cmd_split = ft_split(argv, ' ');
+	if (cmd_split == NULL)
+		error_message(4);
+	env_paths = get_env_path(envp);
+	if (env_paths == NULL)
+	{
+		free(cmd_split);
+		error_message(5);
+	}
+	path = get_path(cmd_split[0], env_paths);
+	if (execve(path, cmd_split, envp) < 0)
+	{
+		free(cmd_split);
+		free(env_paths);
+		error_message(6);
+	}
+}
+
+void	fork_process(char *argv, char *envp[], int *pipefd)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid < 0)
+		error_message(3);
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		execute(argv, envp);
+	}
+	else
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+	}
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	int	fd_in;
+	int	fd_out;
+	int	pipefd[2];
+
+	if (argc < 5)
+		error_message(1);
+	fd_in = open(argv[1], O_RDONLY);
+	dup2(fd_in, STDIN_FILENO);
+	if (pipe(pipefd) == -1)
+		error_message(2);
+	fd_out = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	fork_process(argv[2], envp, pipefd);
+	dup2(fd_out, STDOUT_FILENO);
+	execute(argv[argc - 2], envp);
+	return (EXIT_SUCCESS);
+}
---
+++ pipex/utils.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 10:48:20 by hgandar           #+#    #+#             */
+/*   Updated: 2023/12/10 10:48:34 by hgandar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include "libft/libft.h"
+#include <string.h>
+#include <stdio.h> 
+
+char	**get_env_path(char *envp[])
+{
+	int		i;
+	char	**env_paths;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strstr(envp[i], "PATH") != 0)
+		{
+			env_paths = ft_split(ft_strstr(envp[i], "PATH"), ':');
+			if (env_paths == NULL)
+				error_message(8);
+			return (env_paths);
+		}
+		i++;
+	}
+	error_message(8);
+	return (NULL);
+}
+
+char	*get_path(char *cmd, char *env_paths[])
+{
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (env_paths[i] != NULL)
+	{
+		if (ft_strstr(env_paths[i], cmd) == 0)
+		{
+			path = ft_strjoin(env_paths[i], "/");
+			path = ft_strjoin(path, cmd);
+			if (access(path, F_OK | X_OK) == 0)
+			{
+				//free(exec);
+				//free_all(env_paths);
+				return (path);
+			}
+			//free(path);
+		}
+		i++;
+	}
+	//free(exec);
+	//free_all(env_paths);
+	error_message(9);
+	return (NULL);
+}
+
+char	*ft_strstr(const char *hstack, const char *needle)
+{
+	unsigned long	i;
+	unsigned long	j;
+
+	i = 0;
+	j = 0;
+	if (needle[i] == 0 || needle[j] == 0)
+		return ((char *)hstack);
+	if (hstack == NULL)
+		return (NULL);
+	while (hstack[i])
+	{
+		j = 0;
+		while (needle[j] && needle[j] == hstack[i + j])
+			j++;
+		if (needle[j] == 0)
+			return ((char *)&hstack[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	free_all(char **all)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (all[i])
+		i++;
+	while (j < i)
+	{
+		free(all[j]);
+		j++;
+	}
+	free(all);
+}
+
+void	error_message(int flag)
+{
+	if (flag == 1)
+		perror("Not enough arguments\n");
+	else if (flag == 2)
+		perror("Could not create pipe\n");
+	else if (flag == 3)
+		perror("Could not fork\n");
+	else if (flag == 4)
+		perror("Error splitting the command\n");
+	else if (flag == 5)
+		perror("Error getting env path\n");
+	else if (flag == 6)
+		perror("Error getting execute");
+	else if (flag == 7)
+		perror("Error getting exec split");
+	else if (flag == 8)
+		perror("Error getting env PATH");
+	else if (flag == 9)
+		perror("Error getting PATH");
+	exit(EXIT_FAILURE);
+}
