86f47ff72210b18799d9f05595829d782ca5d5df
Hélène Gandar <helenegandar@ik.me>
2024-02-05 08:07:00 +0100

    so_long

---
+++ so_long/so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 18:15:00 by hgandar           #+#    #+#             */
+/*   Updated: 2024/02/02 18:20:22 by hgandar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <math.h>
+# include "mlx/mlx.h"
+
+
+#endif
