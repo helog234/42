fbcf9ad285675c7dac4fa62ba56ef69c468b2d5b
Hélène Gandar <helenegandar@ik.me>
2024-04-30 11:30:27 +0200

    ajout minishell

---
+++ Philosophers/philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 16:37:45 by hgandar           #+#    #+#             */
+/*   Updated: 2024/04/29 17:45:55 by hgandar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO.H
+# define PHILO.H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <pthread.h>
+
+#endif
