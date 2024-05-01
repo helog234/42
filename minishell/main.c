39de667858157664a833074488b244a42962ea9c
Hélène Gandar <helenegandar@ik.me>
2024-05-01 18:07:18 +0200

    ok

--- minishell/main.c
+++ minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: hgandar <hgandar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 13:43:55 by hgandar           #+#    #+#             */
-/*   Updated: 2024/04/26 12:34:43 by hgandar          ###   ########.fr       */
+/*   Updated: 2024/04/30 14:56:05 by hgandar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ int	main(int argc, char *argv[], char *envp[])
 	(void)argc;
 	ms_env = NULL;
 	head = NULL;
+	if (envp == NULL)
+		return (1);
 	save_env(&ms_env, envp);
 	msh_dir = form_err_msg(getenv("PWD"), "/", ".history.log");
 	load_history(msh_dir);
 	set_signals(head, 0);
 	g_signal_flag = 0;
 	return (set_prompt(ms_env, &head, msh_dir));
-	free(msh_dir);
 }
